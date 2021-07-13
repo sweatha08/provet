860dd529b7196c67430b1034ee89544ce3290287
Elakkiya-tech <67002119+Elakkiya-tech@users.noreply.github.com>
2021-07-13 13:12:07 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import First from './Component/Firstpg/First';
+
+function App() {
+  return (
+    <div className="App">
+     <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Firstpg/First.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class First extends React.Component {
+    render() {
+        return(
+            <div >
+                Hello All
+            </div>
+        );
+    }
+}
+
+export default First;
