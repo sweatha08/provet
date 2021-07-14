bd8a767aa217fe7d7d902f7e3895f867a54b2a94
Elakkiya-tech <67002119+Elakkiya-tech@users.noreply.github.com>
2021-07-14 19:54:44 +0530

    update

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -1,41 +1,38 @@
 import React from 'react';
+import { Grid, Paper, Typography, TextField, Button } from '@material-ui/core'
+// import Img from '../../Images/probg.jpg';
+// import { createTheme } from '@material-ui/core/styles';
+const Register = () => {
 
-function Register() {
-   
-        return(
-            <div >
-                 <form>
-                <h3>Sign Up</h3>
+    const paperStyle = { padding: '30px 35px', width: 375, margin: "140px auto" }
+    const headerStyle = { margin: 0 }
+    const styles = { margin: '8px auto' }
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
 
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
+    return (
+        <div className="sample">
+            <Grid container spacing={40}>
+                <Paper elevation={20} style={paperStyle}>
+                    <Grid align='center'>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+                        <h2 style={headerStyle}>SIGN UP</h2>
+                        <Typography variant='caption' gutterBottom>Please fill this form to create an account</Typography>
+                    </Grid>
+                    <form  >
+                        <TextField style={styles} fullWidth label='Name' placeholder="Enter your name" />
+                        <TextField style={styles} fullWidth label='Email' placeholder="Enter your email" />
+                        <TextField style={styles} fullWidth label='Phone Number' placeholder="Enter your phone number" />
+                        <TextField style={styles} fullWidth label='Password' placeholder="Enter your password" />
+                        <TextField style={styles} fullWidth label='Confirm Password' placeholder="Confirm your password" />
+                        <br /><Button type='submit' variant='contained' color='primary'>Sign up</Button>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
-                </p>
-            </form>
-            </div>
-        );
-    
+                    </form>
+                </Paper>
+            </Grid>
+        </div>
+    );
 }
 
+
 export default Register;
-           
+
